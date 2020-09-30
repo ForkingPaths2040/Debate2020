7103206eab7a6e4cb80f94c7a3bd80c1f3906f59
Otto Rotor <ambermoorexc@gmail.com>
2020-09-30 09:00:50 -0500

    All files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Canidate from './Canidate'
 
 function App() {
+  const trump = ["There's nothing smart about you, Joe", "Proud Boys, stand back and stand by,", "I did more in 47 months as President than Joe Biden did in 47 years!"]
+  const biden = ["It's hard to get any words in with this clown.", "Show us your tax returns.", "Will you shut up, man?"]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canidate
+        who={trump}
+        name="Donald Trump"
+        title="U.S. President"
+        party="Republican"
+        imageUrl="https://media.vanityfair.com/photos/5f73ffc51974fdefdd159e75/master/w_2560%2Cc_limit/donaldtrumpjoebidendebate.jpg"
+      />
+      <Canidate
+        who={biden}
+        name="Joe Biden"
+        title="Former Vice President"
+        party="Democratic"
+        imageUrl="https://static01.nyt.com/images/2020/09/29/us/politics/-29vid-debate-biden-2/-29vid-debate-biden-2--videoSixteenByNine3000-v2.jpg"
+      />
     </div>
   );
 }
---
+++ src/Canidate.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+function Canidate(props) {
+  const [quote, setQuote] = useState('')
+
+
+
+
+  function toggleQuote() {
+    setQuote(props.who[(Math.floor(Math.random() * props.who.length))])
+  }
+  return (
+    <div className="card">
+      <h2>"{quote}"</h2>
+      <img src={props.imageUrl} alt='' />
+      <h3>
+        {props.name}
+      </h3>
+      <h5>
+        {props.title}
+      </h5>
+      <h6>
+        {props.party}
+      </h6>
+      <div>
+        <p>
+          <button onClick={toggleQuote}>Next</button>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Canidate
